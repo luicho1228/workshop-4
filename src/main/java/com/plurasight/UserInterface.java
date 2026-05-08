b7cce90f35d99b2142a6134abd6ed164709fd669
luisvasquezgonzalez <luicho.vasquez44@gmail.com>
2026-05-08 11:21:27 -0400

    implemented the main menu method called display(), this is where the dealership is retrieved from the file and save in the program also this method initialize the main menu

--- src/main/java/com/plurasight/UserInterface.java
+++ src/main/java/com/plurasight/UserInterface.java
@@ -1,14 +1,47 @@
 package com.plurasight;
 
+import java.util.Scanner;
+
 public class UserInterface {
     private Dealership dealership;
 
     public UserInterface(){
-        this.dealership = DealershipFileManager.getDealership();
+        display();
     }
     public void display(){
+        init();
+        boolean isRunning = true;
+        while (isRunning){
+            //display Menu
+            Scanner scanner = new Scanner(System.in);
+            int userInput = scanner.nextInt();
+            switch (userInput){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+            }
+        }
+    }
+    private void init(){
+        this.dealership = DealershipFileManager.getDealership();
 
     }
+
     public void processGetByPriceRequest(){
 
     }
