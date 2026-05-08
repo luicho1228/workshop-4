0734765f421a5a0fc00dd52745e79e601990cbfd
luisvasquezgonzalez <luicho.vasquez44@gmail.com>
2026-05-08 11:01:13 -0400

    Added the dealership-inventory.csv file, implemented the getDealership method that loads the inventory data from dealership-inventory.csv file

--- src/main/java/com/plurasight/DealershipFileManager.java
+++ src/main/java/com/plurasight/DealershipFileManager.java
@@ -1,9 +1,35 @@
 package com.plurasight;
 
+import java.io.*;
+
 public class DealershipFileManager {
 
     public static Dealership getDealership(){
         //loadDealership from file
+        Vehicle vehicle =null;
+        String filePath = "dealership-inventory.csv";
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
+            String fileDataString = bufferedReader.readLine();
+            String[] fileDataArray = fileDataString.split("\\|");
+            Dealership dealership = new Dealership(fileDataArray[0], fileDataArray[1],fileDataArray[2]);
+            while ((fileDataString = bufferedReader.readLine()) != null){
+                fileDataArray = fileDataString.split("\\|");
+                int vin = Integer.parseInt(fileDataArray[0]);
+                int year = Integer.parseInt(fileDataArray[1]);
+                int odometer = Integer.parseInt(fileDataArray[6]);
+                double price = Double.parseDouble(fileDataArray[7]);
+                dealership.addVehicle(new Vehicle(vin,year,fileDataArray[2],fileDataArray[3],fileDataArray[4],fileDataArray[5],odometer,price));
+
+            }
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
         return null;
     }
     public static void saveDealership(Dealership dealership){
